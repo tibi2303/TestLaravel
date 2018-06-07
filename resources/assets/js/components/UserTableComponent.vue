84a6648fb35ab4a1ac48d6abe797a95b9dce2edc
DESKTOP-BS5KHEO\Tibby <tiberiu.dumitru23@gmail.com>
2018-06-07 09:50:37 +0300

    Vue user table created

---
+++ resources/assets/js/components/UserTableComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                users: [],
+                user: {
+                    id: '',
+                    name: '',
+                    email: '',
+                    role: '',
+                    created_at: '',
+                    updated_at: '',
+                },
+                pagination: {}
+            }
+        },
+        created() {
+            this.fetchUsers();
+        },
+        methods: {
+            fetchUsers() {
+                fetch('api/users')
+                    .then(res => res.json())
+                    .then(res => {
+                        console.log(res.data);
+                    })
+            }
+        }
+    }
+
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Test') }}</title>
 
@@ -13,6 +14,7 @@
 <body>
 <div id="app">
     @yield('content')
+    <user-table></user-table>
 </div>
 
 <!-- Scripts -->
